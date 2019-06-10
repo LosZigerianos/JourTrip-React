28c5784b264c7ec616b45d0e6c72cb2a5a7b42cb
DanyST <dany.herrera20_@hotmail.com>
2019-06-10 17:14:38 -0400

    Add react-router and login component default

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './sections/login';
+
+const Routes = () => (
+    <Switch>
+        <Route exact path="/" component={Login} />
+    </Switch>
+)
+
+export default Routes;
+
+export const routes = {
+    login: () => '/'
+}
