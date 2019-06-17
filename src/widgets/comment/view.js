04760b0e4ccfefc1fc2e1ac33865008bc27fd18c
DanyST <dany.herrera20_@hotmail.com>
2019-06-16 23:49:33 -0400

    Add Comment Component

---
+++ src/widgets/comment/view.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import moment from 'moment';
+import './style.css';
+
+
+const Comment = props => (
+    <Card className={`border ${props.containerClassName}`}>
+        {props.renderPhoto()}
+        <Card.Body>
+            <Card.Title>{props.renderUser()}</Card.Title>
+            <Card.Text>{props.details.description}</Card.Text>
+            <Card.Text className="text-right text-secondary">
+                {moment(props.details.creation_date).fromNow()}
+            </Card.Text>
+        </Card.Body>
+    </Card>
+);
+
+Comment.defaultProps = {
+    details: { location: { photos: [] } },
+    containerClassName: '',
+    renderUser: () => {},
+    renderPhoto: () => {},
+};
+
+export default Comment;
--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,4 +1,8 @@
 import LoaderButton from './loaderButton';
 import SVGLogo from './SVGLogo';
+import Comment from './comment';
+import Navbar from './navBar';
+import UserInfo from './userInfo';
+import PhotoInfo from './photoInfo';
 
-export { LoaderButton, SVGLogo };
+export { LoaderButton, SVGLogo, Comment, Navbar, UserInfo, PhotoInfo };
